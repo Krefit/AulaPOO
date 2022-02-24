12e92c48289916e43b2c4b041981985ee9da99ee
Krefit <gustavokrawczyk@gmail.com>
2022-02-23 22:11:32 -0300

    Finalização atividade 4

--- ProjetoPacientePoo/src/Modelos/Funcionario.java
+++ ProjetoPacientePoo/src/Modelos/Funcionario.java
@@ -9,24 +9,22 @@ package Modelos;
  * @author ALUNO
  */
 public class Funcionario {
+
     private String nome = "";
     private float salarioMes = 0;
     private int qtdFilhos = 0;
     private float aumentoSalarial = 0;
-    private float novoSalario = 0;
-    
-    public float getNovoSalario(){
-        return novoSalario;
-    }
-    
+    private float parcelaINSS = 0;
+
     public String getNome() {
         return nome;
     }
 
-    public void setNome(String nome) throws Exception{
+    public void setNome(String nome) throws Exception {
         if (nome.contains("1") || nome.contains("2") || nome.contains("3") || nome.contains("4") || nome.contains("5")
-                || nome.contains("6") || nome.contains("7") || nome.contains("8") || nome.contains("9") || nome.contains("0"))
+                || nome.contains("6") || nome.contains("7") || nome.contains("8") || nome.contains("9") || nome.contains("0")) {
             throw new Exception("Nome contém caracteres inválidos.");
+        }
         this.nome = nome;
     }
 
@@ -34,9 +32,10 @@ public class Funcionario {
         return salarioMes;
     }
 
-    public void setSalarioMes(float salarioMes) throws Exception{
-        if(salarioMes < 0)
-            throw new Exception ("Valor de salário invalido.");
+    public void setSalarioMes(float salarioMes) throws Exception {
+        if (salarioMes < 0) {
+            throw new Exception("Valor de salário invalido.");
+        }
         this.salarioMes = salarioMes;
     }
 
@@ -44,49 +43,55 @@ public class Funcionario {
         return qtdFilhos;
     }
 
-    public void setQtdFilhos(int qtdFilhos) throws Exception{
-        if(qtdFilhos < 0)
+    public void setQtdFilhos(int qtdFilhos) throws Exception {
+        if (qtdFilhos < 0) {
             throw new Exception("Quantidade de filhos não pode ser menor que zero.");
+        }
         this.qtdFilhos = qtdFilhos;
     }
 
-    public float getAumentoSalarial() {
-        return aumentoSalarial;
+    public float calculaAumentoSalarial(float aumentoSalarial) {
+        float novoSalario = 0;
+
+        novoSalario = salarioMes + salarioMes * (aumentoSalarial / 100);
+        this.aumentoSalarial = novoSalario;
+        return novoSalario;
     }
 
-    public void setAumentoSalarial(float aumentoSalarial) throws Exception{
-        if(aumentoSalarial < 0)
-            throw new Exception("Valor de aumento salarial invalido.");
-        this.aumentoSalarial = aumentoSalarial;
-    }
-    
-    
-    
-    public float calculaAumentoSalarial(){
-        float aumentoSal = 0;
-        
-        aumentoSal = salarioMes * (aumentoSalarial/100);
-        novoSalario = aumentoSal;
-        return aumentoSal;
-    }
-    
-    public float calculaINSS(){
-        float inss = 0;
-        
-        if(novoSalario <= 3545.00f)
-            inss = novoSalario * 0.06f;
-        else
-            inss = novoSalario * 0.1f;
-        
+    public String calculaINSS() {
+        String inss = "";
+        float varCalcula = 0;
+
+        if (aumentoSalarial <= 2903.98f) {
+            inss = "Isento";
+        } else if (aumentoSalarial < 3826.65f) {
+            varCalcula = aumentoSalarial * 0.075f;
+            inss = Float.toString(varCalcula);
+        } else if (aumentoSalarial < 4751.05f) {
+            varCalcula = aumentoSalarial * 0.15f;
+            inss = Float.toString(varCalcula);
+        } else if (aumentoSalarial < 5664.68f) {
+            varCalcula = aumentoSalarial * 0.225f;
+            inss = Float.toString(varCalcula);
+        } else {
+            varCalcula = aumentoSalarial * 0.275f;
+            inss = Float.toString(varCalcula);
+        }
+        parcelaINSS = varCalcula;
+        //inss = Float.toString(varCalcula);
+
         return inss;
     }
     
-    public float calculaParcelaIR(){
-        float parIR = 0;
+    public float calculaIRPF(){
+        float irpf = 0;
         
+        irpf = parcelaINSS-(qtdFilhos * 145f);
         
+        if(irpf < 0)
+            return 0;
         
-        return parIR;
+        return irpf;
     }
-    
+
 }
