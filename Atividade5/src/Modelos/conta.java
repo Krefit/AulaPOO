6fb889e734fb49dc562a62b0a1eff5f615d3af03
Krefit <gustavokrawczyk@gmail.com>
2022-03-04 22:11:08 -0300

    Criação atividade 5

---
+++ Atividade5/src/Modelos/conta.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author gusta
+ */
+public class conta {
+
+    private String nomeCliente = "";
+    private int numeroConta = 0;
+    private int numeroAgencia = 0;
+    private int saldo = 0;
+
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+
+    public void setNomeCliente(String nomeCliente) {
+        this.nomeCliente = nomeCliente;
+    }
+
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+
+    public int getNumeroAgencia() {
+        return numeroAgencia;
+    }
+
+    public void setNumeroAgencia(int numeroAgencia) {
+        this.numeroAgencia = numeroAgencia;
+    }
+
+    public int getSaldo() {
+        return saldo;
+    }
+
+    public float sacar(int valor) throws Exception {
+        if (valor <= 0) {
+            throw new Exception("valor não pode ser <= 0");
+        }
+        if (valor >= saldo) {
+            throw new Exception("sem fundo");
+        }
+        saldo -= valor;
+        return valor;
+    }
+
+    public void depositar(int valor) throws Exception {
+        if (valor <= 0) {
+            throw new Exception("o valor nao pode ser <=0");
+        }
+        saldo += valor;
+    }
+}
